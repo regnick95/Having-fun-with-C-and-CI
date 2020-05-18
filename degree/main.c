994c10169cfe3ec9cfc3c7372d71423c557c84d2
regnick95 <rsyln001@fiu.edu>
2020-05-18 12:31:21 -0500

    Added test cases

--- degree/main.c
+++ degree/main.c
@@ -1,21 +1,29 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
+#include <CUnit/CUnit.h>
+#incude <CUnit/Basic.h>
 #include <stdio.h>
 #include "degree.h"
 
+void test_degeeconv(){
+  CU_ASSERT(cel_to_far(28) == 82);
+  CU_ASSERT(far_to_cel(82) == 28);
+  CU_ASSERT(cel_to_far(0) == 32);
+  CU_ASSERT(far_to_cel(32) == 0);
+  CU_ASSERT(cel_to_far(100) == 212);
+  CU_ASSERT(far_to_cel(212) == 100);
+}
+
 int main()
 {
-    int c_degree = 28;
-    int f_degree = 85;
+    //int c_degree = 28;
+    //int f_degree = 85;
     
-    printf("%d° celcius is equal to %d° fahrenheit\n", c_degree, cel_to_far(c_degree));
-    printf("%d° fahrenheit is equal to %d° celcius\n", f_degree, far_to_cel(f_degree));
+    //#printf("%d° celcius is equal to %d° fahrenheit\n", c_degree, cel_to_far(c_degree));
+    //#printf("%d° fahrenheit is equal to %d° celcius\n", f_degree, far_to_cel(f_degree));
+  CU_initialize_registry();
+  CU_pSuite suite = CU_add_suite("conversion test suite",0,0);
+  CU_add_test(suite, "conversion test", test_degreeconv);
+  CU_basic_run_tests();
+  CU_cleanup_registry();
 
     return 0;
 }
