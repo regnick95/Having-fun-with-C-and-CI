421970029ae47c0b3505afcc26c104312b532251
regnick95 <rsyln001@fiu.edu>
2020-05-18 17:19:46 -0500

    Update CUnit_activity3.c

--- CUnit_activity3.c
+++ CUnit_activity3.c
@@ -10,44 +10,6 @@ typedef struct{
   char unit;        //'f' 'c' 'k' -> fahrenheit, celius and kelvin
 } _degree;
 
-/*hard coded input*/
-   _degree degree1;
-   degree1.value = 34;
-   degree1.unit = 'c';
-   
-   _degree degree2;
-   degree2.value = 98;
-   degree2.unit = 'f';
-   
-   _degree degree3;
-   degree3.value = 205;
-   degree3.unit = 'k';
-   
-    _degree degree4;
-   degree4.value = 18;
-   degree4.unit = 'c';
-   
-   _degree degree5;
-   degree5.value = 25;
-   degree5.unit = 'f';
-   
-   _degree degree6;
-   degree6.value = 117;
-   degree6.unit = 'k';
-   
-    _degree degree7;
-   degree7.value = 34;
-   degree7.unit = 'c';
-   
-   _degree degree8;
-   degree8.value = 98;
-   degree8.unit = 'f';
-   
-   _degree degree9;
-   degree9.value = 205;
-   degree9.unit = 'k';
-
-
 /**
 * This function prints the value of a degree and its unit.
 * @param degree The degree structure pointer.
@@ -118,7 +80,45 @@ void test_degeeconv(void){
 
 
 int main()
-{  
+{ 
+  /*hard coded input*/
+   _degree degree1;
+   degree1.value = 34;
+   degree1.unit = 'c';
+   
+   _degree degree2;
+   degree2.value = 98;
+   degree2.unit = 'f';
+   
+   _degree degree3;
+   degree3.value = 205;
+   degree3.unit = 'k';
+   
+    _degree degree4;
+   degree4.value = 18;
+   degree4.unit = 'c';
+   
+   _degree degree5;
+   degree5.value = 25;
+   degree5.unit = 'f';
+   
+   _degree degree6;
+   degree6.value = 117;
+   degree6.unit = 'k';
+   
+    _degree degree7;
+   degree7.value = 34;
+   degree7.unit = 'c';
+   
+   _degree degree8;
+   degree8.value = 98;
+   degree8.unit = 'f';
+   
+   _degree degree9;
+   degree9.value = 205;
+   degree9.unit = 'k';
+  
+  
   CU_initialize_registry();
   CU_pSuite suite = CU_add_suite("conversion test suite",0,0);
   CU_add_test(suite, "conversion test", test_degeeconv);
